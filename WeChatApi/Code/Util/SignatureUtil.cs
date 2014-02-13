4ab9705ae8564da2ed3d9d0f1f7e093c577af29e
Jim <developer.lao@gmail.com>
2014-02-14 00:24:35 +0800

    --wechat CheckSignature

---
+++ WeChatApi/Code/Util/SignatureUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WeChatApi.Code.Util
+{
+    public class SignatureUtil
+    {
+        public static readonly string Token = System.Configuration.ConfigurationManager.AppSettings["Token"];
+
+        public static bool CheckSignature(string signature, string timestamp, string nonce)
+        {
+            return signature == GetSignature(timestamp, nonce);
+        }
+
+        public static string GetSignature(string timestamp, string nonce)
+        {
+            var arr = new[] { Token, timestamp, nonce }.OrderBy(z => z).ToArray();
+            var arrString = string.Join("", arr);
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var sha1Arr = sha1.ComputeHash(Encoding.UTF8.GetBytes(arrString));
+            StringBuilder enText = new StringBuilder();
+            foreach (var b in sha1Arr)
+            {
+                enText.AppendFormat("{0:x2}", b);
+            }
+
+            return enText.ToString();
+        }
+    }
+}
---
+++ WeChatApi/Controllers/WeChatController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Xml;
+using System.Xml.Linq;
+using WeChatApi.Code.Util;
+
+namespace WeChatApi.Controllers
+{
+    public class WeChatController : ApiController
+    {
+        public HttpResponseMessage Get(string signature, string timestamp, string nonce, string echostr)
+        {
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            if (SignatureUtil.CheckSignature(signature, timestamp, nonce))
+            {
+                responseMessage.Content = new StringContent(echostr);
+            }
+            else
+            {
+                responseMessage.Content = new StringContent("signature failed:" + signature);
+            }
+
+            return responseMessage;
+        }
+
+        public  HttpResponseMessage Post(string signature, string timestamp, string nonce)
+        {
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            if (!SignatureUtil.CheckSignature(signature, timestamp, nonce))
+            {
+                responseMessage.Content = new StringContent("参数错误!");
+            }
+
+            XDocument doc = new XDocument();
+
+            using (XmlReader xr = XmlReader.Create(HttpContext.Current.Request.InputStream))
+            {
+                doc = XDocument.Load(xr);
+            }
+
+            //TODO: logic code here
+
+            return responseMessage;
+        }
+    }
+}
